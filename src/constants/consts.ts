8cbd0015a6ae01024b42dbebc298ad8ce80abdd0
akmatoff <akmatoff.exe@gmail.com>
2022-04-05 19:04:25 +0600

    handle input state and actual state separately | set max and min values if doesnt fit

--- src/constants/consts.ts
+++ src/constants/consts.ts
@@ -1,7 +1,7 @@
-export const MAX_RACKS_COUNT = 12;
-export const MAX_BPM = 200;
-export const MIN_BPM = 30;
-export const MAX_BEATS_MEASURE = 8;
-export const MIN_BEATS_MEASURE = 3;
-export const MAX_BARS = 32;
-export const MIN_BARS = 4;
+export const MAX_RACKS_COUNT: number = 12;
+export const MAX_BPM: number = 200;
+export const MIN_BPM: number = 30;
+export const MAX_BEATS_MEASURE: number = 8;
+export const MIN_BEATS_MEASURE: number = 3;
+export const MAX_BARS: number = 32;
+export const MIN_BARS: number = 4;
--- src/pages/Home/components/Header/Header.tsx
+++ src/pages/Home/components/Header/Header.tsx
@@ -1,14 +1,19 @@
-import { FC, FormEvent } from "react";
+import { FC, FormEvent, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import styled from "styled-components";
 import { bindActionCreators } from "redux";
 
 import { AppDispatch, RootState } from "../../../../store/store";
-import { MAX_BARS, MAX_BEATS_MEASURE, MAX_BPM, MAX_RACKS_COUNT, MIN_BARS, MIN_BPM } from "../../../../constants/consts";
+import { MAX_BARS, MAX_BEATS_MEASURE, MAX_BPM, MAX_RACKS_COUNT, MIN_BARS, MIN_BEATS_MEASURE, MIN_BPM } from "../../../../constants/consts";
 import * as audioActions from "../../../../store/audio/actions";
 
 const Header: FC = () => {
-    const { bpm, bars, beatsMeasure, racks } = useSelector((state: RootState) => state.audio)
+
+    const { bpm, beatsMeasure, bars, racks } = useSelector((state: RootState) => state.audio)
+
+    const [bpmInput, setBpmInput] = useState<number>(bpm)
+    const [beatsMeasureInput, setBeatsMeasureInput] = useState<number>(beatsMeasure)
+    const [barsInput, setBarsInput] = useState<number>(bars)
 
     const dispatch: AppDispatch = useDispatch()
 
@@ -16,29 +21,50 @@ const Header: FC = () => {
 
     const onBpmChange = (e: FormEvent<HTMLInputElement>) => {
         let value = +e.currentTarget.value
-        if (isNaN(value) ||
-            value > MAX_BPM ||
-            value < MIN_BPM) return;
+        if (isNaN(value)) return;
 
-        changeBpm(value)
+        setBpmInput(value)
     }
-
+       
     const onBeatsMeasureChange = (e: FormEvent<HTMLInputElement>) => {
         let value = +e.currentTarget.value
-        if (isNaN(value) ||
-            value > MAX_BEATS_MEASURE ||
-            value < MAX_BEATS_MEASURE) return;
+        if (isNaN(value)) return;
         
-        changeBeatsMeasure(value)
+        setBeatsMeasureInput(value)
     }
 
     const onBarsChange = (e: FormEvent<HTMLInputElement>) => {
         let value = +e.currentTarget.value
-        if (isNaN(value) ||
-            value > MAX_BARS ||
-            value < MIN_BARS) return;
+        if (isNaN(value)) return;
         
+        setBarsInput(value)
+    }
+
+    const onBpmBlur = (e: FormEvent<HTMLInputElement>) => {
+        let value = +e.currentTarget.value
+        if (value > MAX_BPM) value = MAX_BPM
+        else if (value < MIN_BPM) value = MIN_BPM 
+
+        changeBpm(value)
+        setBpmInput(value)
+    }
+
+    const onBeatsMeasureBlur = (e: FormEvent<HTMLInputElement>) => {
+        let value = +e.currentTarget.value
+        if (value > MAX_BEATS_MEASURE) value = MAX_BEATS_MEASURE
+        else if (value < MIN_BEATS_MEASURE) value = MIN_BEATS_MEASURE
+
+        changeBeatsMeasure(value)
+        setBeatsMeasureInput(value)
+    }
+
+    const onBarsBlur = (e: FormEvent<HTMLInputElement>) => {
+        let value = +e.currentTarget.value 
+        if (value > MAX_BARS) value = MAX_BARS
+        else if (value < MIN_BARS) value = MIN_BARS
+
         changeBars(value)
+        setBarsInput(value)
     }
 
     return <StyledHeader>
@@ -46,17 +72,17 @@ const Header: FC = () => {
         <InfoWrapper>
             <InfoContainer>
                 <InfoTitle>BPM</InfoTitle>
-                <InfoInput value={bpm} onChange={onBpmChange} />
+                <InfoInput value={bpmInput} onChange={onBpmChange} onBlur={onBpmBlur} />
             </InfoContainer>
 
             <InfoContainer>
                 <InfoTitle>Beats Measure</InfoTitle>
-                <InfoInput value={beatsMeasure} onChange={onBeatsMeasureChange} />
+                <InfoInput value={beatsMeasureInput} onChange={onBeatsMeasureChange} onBlur={onBeatsMeasureBlur} />
             </InfoContainer>
 
             <InfoContainer>
                 <InfoTitle>Bars</InfoTitle>
-                <InfoInput value={bars} onChange={onBarsChange} />
+                <InfoInput value={barsInput} onChange={onBarsChange} onBlur={onBarsBlur} />
             </InfoContainer>
 
             <InfoContainer>
